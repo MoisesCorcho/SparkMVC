42476432b8b49db91608b1c61ba36df001d48543
MoisesCorcho <55525094+MoisesCorcho@users.noreply.github.com>
2023-09-28 16:00:55 -0500

    first commit

---
+++ Core/Router.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * Router
+ * 
+ * PHP version 8.1.6
+ */
+class Router 
+{
+
+    /**
+     * Associative array of routes (the routing table)
+     *
+     * @var array
+     */
+    protected $routes = [];
+
+    /**
+     * Parameter from the matched route
+     *
+     * @var array
+     */
+    protected $params = [];
+
+    /**
+     * Add a route to the routing table
+     *
+     * @param string $route
+     * @param array $params
+     * 
+     * @return void
+     */
+    public function add($route, $params)
+    {
+        $this->routes[$route] = $params;
+    }
+
+    /**
+     * Get all the routes from the routing table
+     *
+     * @return array
+     */
+    public function getRoutes()
+    {
+        return $this->routes;
+    }
+
+    /**
+     * Match the route to the routes in the routing table, setting the $params
+     * property if a route is found
+     *
+     * @param string $url
+     * @return boolean true if a match found, false otherwise
+     */
+    public function pairing($url)
+    {
+        foreach ($this->routes as $route => $params) {
+            if ($url == $route) {
+                $this->params = $params;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the currently matched parameters
+     *
+     * @return array
+     */
+    public function getParams()
+    {   
+        return $this->params;
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,32 @@
+<?php 
+/**
+ * Front Controller
+ * 
+ * PHP version 8.1.6
+ */
+
+/**
+ * Routing
+ */
+require_once '../Core/Router.php';
+
+$router = new Router();
+
+// Add the routes
+$router->add('', ['controller' => 'Home', 'action' => 'index']);
+$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
+$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+
+// echo '<pre>';
+// var_dump($router->getRoutes());
+// echo '</pre>';
+
+$url = $_SERVER['QUERY_STRING'];
+
+if ($router->pairing($url)) {
+    echo '<pre>';
+    var_dump($router->getParams());
+    echo '</pre>';
+} else {
+    echo 'No route found for URL '. $url;
+}
